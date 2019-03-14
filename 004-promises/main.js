af6a222510706271c7e42e6417bb82d00e0c72e4
Arthur Eichelberger <eichelbergerarthur@gmail.com>
2019-03-14 21:06:11 +0100

    004 - playing with promises

---
+++ 004-promises/main.js
@@ -0,0 +1,55 @@
+const colors = require('colors')
+const logSync = (data) => {
+    console.log(colors.cyan.bold('SYNCHRONOUS : '), data)
+}
+const logAsync = (data) => {
+    console.log(colors.green.bold('ASYNCHRONOUS : '), data)
+}
+const logInfo = (data) => {
+    console.log(colors.red.bold('INFO : '), data)
+}
+
+const getCurrentTime = () => {
+    const date = new Date()
+    return `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds().toString().padStart(2, '0')}`
+}
+
+console.log("\n")
+
+
+const getPromise = (time = 1000) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Resolved.')
+    }, time)
+})
+
+const synchronous = () => {
+    const promises = []
+    for (let i = 0; i < 5; i++) {
+        promises.push(getPromise(i * 500))
+    }
+    promises.forEach(promise => {
+        promise.then(v => logSync(`${getCurrentTime()} : ${v} `))
+    })
+    Promise.all(promises).then(values => {
+        logSync([values, getCurrentTime()])
+    })
+}
+
+const asynchronous = async () => {
+    const promises = []
+    for (let i = 0; i < 5; i++) {
+        promises.push(getPromise(i * 500))
+    }
+    promises.forEach(async promise => {
+        logAsync(await promise)
+    })
+    logAsync([await Promise.all(promises), getCurrentTime()])
+}
+
+
+
+logInfo(getCurrentTime())
+synchronous()
+asynchronous()
+
